2642efe3b6441ffdcf22abbcf172d2d26361e0a4
valviveros <blkfthrs@gmail.com>
2020-09-20 23:19:48 -0500

    Se han empezado animaciones al hacer scroll.

--- src/app/pages/Portfolio/skills.ts
+++ src/app/pages/Portfolio/skills.ts
@@ -1,14 +1,29 @@
+import { update } from 'lodash';
 // Se hace importación de las librerías que se necesitan
 import { EasyPieChart } from './easyPieChart';
 var element: any = document.querySelectorAll(".chart");
-element.forEach(function(element: any) {
-  new EasyPieChart(element, {
+const skillsCont: any = document.querySelector(".skillsContainer");
+var chart: any;
+
+element.forEach(function (element: any) {
+  chart = new EasyPieChart(element, {
     size: 160,
     barColor: "#FF84E8",
     scaleLength: 0,
     lineWidth: 15,
     trackColor: "#373737",
     lineCap: "circle",
-    animate: ({ duration: 4000, enabled: true })
+    animate: ({ duration: 1000, enabled: true })
   });
 });
+
+window.onscroll = () => {
+  console.log("entré");
+  console.log(window.scrollY);
+  element.forEach(function (element: any) {
+    if (window.scrollY >= 825) {
+      skillsCont.style.opacity = 1;
+      element.chart.update(4000);
+    }
+  });
+};
